0fed7a8c33b6c6e7bfffc28df21c1fc248592be5
Sajini-Sayyad <157812760+Sajini-Sayyad@users.noreply.github.com>
2026-06-17 00:31:02 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -91,7 +91,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    disk_size_gb         = 10
+    disk_size_gb         = 30
   }
 
   source_image_reference {
